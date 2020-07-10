41646fbecb64183bdd8e3cb43a71f39092fa88ed
sanjay <sanapplegates@gmail.com>
2020-07-10 13:45:08 +0530

    Exploratory data Analysis

---
+++ docs/index.rst
@@ -0,0 +1,24 @@
+.. indian-startup-funding documentation master file, created by
+   sphinx-quickstart.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+indian-startup-funding documentation!
+==============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   getting-started
+   commands
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project tries to explore few of the below mentioned questions Interested in the Indian startup ecosystem just like me? Wanted to know what type of startups are getting funded in the last few years? Wanted to know who are the important investors? Wanted to know the hot fields that get a lot of funding these days?',
+    author='sanjay',
+    license='MIT',
+)
